9e3b730ea5cfdf41a5066da67d4aa086774d4f70
gavguy <gavguy3@gmail.com>
2019-02-18 14:27:25 +0200

    мелочи ggf

--- src/main/java/apple/service/core/phone/phoneboundary/DeleteBean.java
+++ src/main/java/apple/service/core/phone/phoneboundary/DeleteBean.java
@@ -6,34 +6,26 @@ import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 import java.io.Serializable;
 import java.util.List;
 
 @Named
 @SessionScoped
-public class ListBean implements Serializable {
+public class DeleteBean implements Serializable {
     @PersistenceContext
     private EntityManager em;
-    private String term;
+    private boolean delete = false;
 
-    //vedj tak nado
     private Long id;
 
-    public List<PhoneEntity> getPhones() {
-        if (term == null) {
-            return em.createQuery("select p from Phone p", PhoneEntity.class)
-                    .getResultList();
 
-        } else {
-
-            return em.createQuery("select p from Phone p where lower(p.owner) like :term",
-                    PhoneEntity.class)
-                    .setParameter("term", "%" + term.toLowerCase() + "%")
-                    .getResultList();
-        }
+    public String phonedelete() {
+        PhoneEntity phone;
+        phone = em.find(PhoneEntity.class, id);
+        em.remove(phone);
+        delete = true;
+        return null;
     }
-
 
     public EntityManager getEm() {
         return em;
@@ -43,7 +35,14 @@ public class ListBean implements Serializable {
         this.em = em;
     }
 
- 
+    public boolean isDelete() {
+        return delete;
+    }
+
+    public void setDelete(boolean delete) {
+        this.delete = delete;
+    }
+
     public Long getId() {
         return id;
     }
@@ -52,13 +51,6 @@ public class ListBean implements Serializable {
         this.id = id;
     }
 
-    public String getTerm() {
-        return term;
-    }
-
-    public void setTerm(String term) {
-        this.term = term;
-    }
 
     public void doSearch() {
         System.out.println("Searching");
